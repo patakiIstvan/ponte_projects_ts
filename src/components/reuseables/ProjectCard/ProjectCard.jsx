e5f67ca9fd768d7c625fd19c06a37601d8cb010d
Pataki István <patakiistvan1997y@gmail.com>
2023-09-04 23:35:10 +0200

    extended readme

--- src/components/reuseables/ProjectCard/ProjectCard.jsx
+++ src/components/reuseables/ProjectCard/ProjectCard.jsx
@@ -4,10 +4,15 @@ import './projectCard.scss';
 
 const ProjectCard = (props) => {
 
+  const titleUrl = props.title.replace(" ", "-")
+
   return (
     <Card>
       <Card.Body>
+        <div className="card-title-container">
+        <img className="lettered-avatar" src={"https://avatar.oxro.io/avatar.svg?name=" + titleUrl} />
         <Card.Title>{props.title}</Card.Title>
+        </div>
         <Card.Text>{props.description}</Card.Text>
       </Card.Body>
     </Card>
