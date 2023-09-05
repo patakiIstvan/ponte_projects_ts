d307817cbbe8a7d34822647ca1364dfe002a3eac
Pataki István <patakiistvan1997y@gmail.com>
2023-09-05 08:40:15 +0200

    message

--- src/components/layout/Navbar/MainNavbar.jsx
+++ src/components/layout/Navbar/MainNavbar.jsx
@@ -1,10 +1,15 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useEffect } from 'react'
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 const MainNavbar = forwardRef(function MainNavbar(props, ref) {
+
+  useEffect(() => {
+    alert("Sajnos nem sikerült hétfőre befejezni a beadandó projektetm azonban az >>absztrakt<< wizard form kész van és van benne validáció. A hét folyamán igyekszem befejezni a projektet.")
+  }, [])
+
   return (
     <Navbar data-bs-theme="dark" className="bg-body-tertiary">
       <Container>
