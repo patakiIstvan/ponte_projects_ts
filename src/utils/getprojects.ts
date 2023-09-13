e2f76f76139f573ab1b288eb49a62bb0199ed1e2
Pataki István <patakiistvan1997y@gmail.com>
2023-09-13 21:31:52 +0200

    removed debugger

---
+++ src/utils/getprojects.ts
@@ -0,0 +1,19 @@
+import { initialProjects } from "./dummdata";
+
+type ProjectType = () => Record<string, any>
+
+export const getProjects: ProjectType = async function () {
+  let projects = localStorage.getItem("projects");
+  if (!projects) {
+    setLocalStorage()
+    return getProjects();
+  } else {
+    projects = JSON.parse(projects);
+  }
+  await new Promise<void>(resolve => setTimeout(resolve, 0));
+  return projects;
+}
+
+function setLocalStorage(data = null) {
+  localStorage.setItem("projects", JSON.stringify(data ? data : initialProjects));
+}
