5ae2afdce11d2f5b5d849f4b07b839c1d4212307
Pataki István <patakiistvan1997y@gmail.com>
2023-09-13 21:33:02 +0200

    turned dummydata to ts file

---
+++ src/utils/dummdata.ts
@@ -0,0 +1,64 @@
+export const dummy_roles = ["Project manager", "Sr. front-end", "Front-end", "Sr. Back-end", "Back-end", "Team leader", "HR", "CEO", "Scrum master", "Marketing"]
+
+export const initialProjects = [
+  {
+    title: "Beadandó feladat átnézése",
+    description: "Senior front-endes kolléga bevonása a beadandó feladat átnézésére.",
+    members:
+      [
+        {
+          name: "Tóth Miska",
+          role: ["Sr. front-end"]
+        },
+        {
+          name: "Veperdi Dóra",
+          role: ["Team leader"]
+        }
+      ],
+    links: ["https://github.com/patakiIstvan/ts_projects_dashboard", "https://patakiistvan.netlify.app/"]
+  },
+  {
+    title: "Pataki István felvétele",
+    description: "Az interjúk után megbizonyosodva a jelölt szakértelméről, Pataki István felvétele a csapatba.",
+    members:
+      [
+        {
+          name: "Gál Judit",
+          role: ["HR"]
+        },
+        {
+          name: "Balázs Bertalan",
+          role: ["CEO"]
+        },
+        {
+          name: "Tóth Miska",
+          role: ["Sr. front-end"]
+        }
+      ],
+    links: ["https://www.facebook.com/istvan.pataki.39/", "https://patakiistvan.netlify.app/"]
+  },
+  {
+    title: "Integráció",
+    description: "Kódbázis bemutatása Pataki Istvánnak, amelynek mélyebb tanulmányozásával hatékonyan tud dolgozni.",
+    members:
+      [
+        {
+          name: "Pataki István",
+          role: ["Front-end"]
+        },
+        {
+          name: "Tóth Miska",
+          role: ["Sr. front-end"]
+        },
+        {
+          name: "Veperdi Dóra",
+          role: ["Team leader"]
+        },
+        {
+          name: "Kiss Sára",
+          role: ["UI/UX designer"]
+        }
+      ],
+    links: ["https://patakiistvan.netlify.app/"]
+  }
+]
